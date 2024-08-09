b19f48c770988f193c63a9b45432523e60f3cbb3
turn_wind <the_honor__@outlook.com>
2024-08-09 15:36:54 +0800

    ini

--- BOPRO.py
+++ BOPRO.py
@@ -35,56 +35,67 @@ def Getvalue(ch,str):
     return str[s+1:s+e+1]
 
 def warm_strat(num):
-    pt = (
-        "You need to assume {} initial points for an optimization problem, and the objective function corresponding to the initial points should be as small as possible."
-        "The objective function of this task is a binary quadratic function."
-        "x1 must be within the range of [-5, 5]. x2 must be within the range of [-5, 5]."
-        "According to the experience of previous tasks, the minimum value of the objective function may be around x1, x2 = 3, 3."
-        "Please do not provide duplicate values."
-        "Please give your answer and format your output as follows: *[],[],[],...,[]*").format(num)
-    datas = Getvalue("*",chat(pt))
-    datas = "[" + datas + "]"
-    datas =  ast.literal_eval(datas)
-    print(datas)
+    flag  = 1 #Determine whether the output meets the format.
+    while flag:
+        pt = (
+            "You need to assume {} initial points for an optimization problem, and the objective function corresponding to the initial points should be as small as possible."
+            "The objective function of this task is a binary quadratic function."
+            "x1 must be within the range of [-5, 5]. x2 must be within the range of [-5, 5]."
+            "According to the experience of previous tasks, the minimum value of the objective function may be around x1, x2 = 3, 3."
+            "Please do not provide duplicate values."
+            "Please give your answer and format your output as follows: *[],[],[],...,[]*").format(num)
+        datas = Getvalue("*",chat(pt))
+        datas = "[" + datas + "]"
+        datas =  ast.literal_eval(datas)
+        if len(datas) == num:
+            flag = 0
+    return datas
 
 def candidate_sampling(history,num):
-    # history = [{params: [1,2], loss: 3}, {params: [2,3], loss: 4}]
-    pt = (
-        "Based on the previous optimization results {}, you need to provide {} candidate points for the next optimization."
-        "The objective function of this task is a binary quadratic function."
-        "x1 must be within the range of [-5, 5]. x2 must be within the range of [-5, 5]."
-        "Please do not provide duplicate values."
-        "Please give your answer and format your output as follows: *[],[],[],...,[]*").format(json.dumps(history),num)
-    datas = Getvalue("*",chat(pt))
-    datas = "[" + datas + "]"
-    datas =  ast.literal_eval(datas)
-    print(datas)
-history = [{"params": [1,2], "loss": 3}, {"params": [2,3], "loss": 4}]
-candidate_sampling(history,5)
-def SurrogateModel(num,samples):
-    for i in range(num):
-        pt = ("You are helping tune hyperparameters to minimize loss."
-              "The objective function of this task is a binary quadratic function."
-              "You need to guess the target function value for a given x based on historical evaluation data."
-              "Below is the historical evaluation data, formatted as [Hyperparameters] - [loss]:"
-              "x: [0, 0] - [loss: 13]"
-              "Please guess the loss for x:{} and format your output as follows: *[loss: xx]*").format("[1, 1]")
-        perf = Getvalue("*",chat(pt))
-        print(perf)
+    flag  = 1 #Determine whether the output meets the format.
+    while flag:
+        pt = (
+            "Based on the previous optimization results {}, you need to provide {} candidate points for the next optimization."
+            "The objective function of this task is a binary quadratic function."
+            "x1 must be within the range of [-5, 5]. x2 must be within the range of [-5, 5]."
+            "Please do not provide duplicate values."
+            "Please give your answer and format your output as follows: *[],[],[],...,[]*").format(json.dumps(history),num)
+        datas = Getvalue("*",chat(pt))
+        datas = "[" + datas + "]"
+        datas =  ast.literal_eval(datas)
+        if len(datas) == num:
+            flag = 0
+    return datas
+    
+def SurrogateModel(history,samples):
+    data_pred = []
+    for i in range(len(samples)):
+        flag  = 1 #Determine whether the output meets the format.
+        while flag:
+            pt = ("You are helping tune hyperparameters to minimize loss(>0)."
+                "The objective function of this task is a binary quadratic function."
+                "You need to guess the target function value for a given x based on historical evaluation data."
+                "Below is the historical evaluation data, formatted as [Hyperparameters] - [loss]:"
+                "{}"
+                "Please guess the loss for params:{} and format your output as follows: *xx*").format(history,samples[i])
+            loss = re.findall(r'-?\d+\.\d+|-?\d+', Getvalue("*",chat(pt)))
+            if len(loss) == 1:
+                flag = 0
+                new_entry = {"params": samples[i], "loss": float(loss[0])}
+                data_pred.append(new_entry)
+    return data_pred
 
-# while True:
-#     str = chat(says)
-#     s = str.find("{")
-#     e = str.find("}")
-#     str = str[s:e+1]
-#     data = json.loads(str)
-#     numbers = list(data.values())
-#     loss = obj(numbers[0][0],numbers[0][1])
-#     loss = np.round(loss,2)
-#     text  = str + " - [\"loss\": {}]".format(loss)
-#     says = (says[:-198] + "\n"+ text  +says[-198:])
-#     print("user: "+says)
-#     if loss == 0:
-#         break
-# warm_strat(5)
-SurrogateModel(1,None)
+
+
+if __name__ == "__main__":
+    inidatas = warm_strat(3)
+    datas = []
+    for i in inidatas:
+        loss = obj(i[0],i[1])
+        new_entry = {"params": [i[0], i[1]], "loss": loss}
+        datas.append(new_entry)
+        
+    samplers = candidate_sampling(datas,5)
+    data_pred = SurrogateModel(datas,samplers)
+    
+        
