72c48d37a6e600112885159d8b1c9a9cf03fa95b
feng-arch <72989129+feng-arch@users.noreply.github.com>
2024-08-14 16:29:04 +0800

    第一个最终版本

--- EXP/exp_findmin/cubic_function/plot_data.py
+++ EXP/exp_findmin/cubic_function/plot_data.py
@@ -42,19 +42,23 @@ for i in range(len(data_LLM[0])):
 for i in range(len(data_BOPRO[0])):
     std_BOPRO.append(np.std(data_BOPRO[:, i])*scaler)
 
+plt.grid(True)
+
 # 绘制误差线
 x_values = np.arange(len(data_RS[0]))
-plt.plot(x_values, average_RS, label='RS')
-plt.fill_between(x_values, np.array(average_RS) - np.array(std_RS), np.array(average_RS) + np.array(std_RS), alpha=0.3)
+plt.plot(x_values, average_RS, label='RS', color='gray', linestyle='-.')
+plt.fill_between(x_values, np.array(average_RS) - np.array(std_RS), np.array(average_RS) + np.array(std_RS), alpha=0.3, color='gray')
 x_values = np.arange(len(data_BO[0]))
-plt.plot(x_values, average_BO, label='BO')
-plt.fill_between(x_values, np.array(average_BO) - np.array(std_BO), np.array(average_BO) + np.array(std_BO), alpha=0.3)
+plt.plot(x_values, average_BO, label='BO',color='green', linestyle='--')
+plt.fill_between(x_values, np.array(average_BO) - np.array(std_BO), np.array(average_BO) + np.array(std_BO), alpha=0.3, color='green')
 x_values = np.arange(len(data_LLM[0]))
 plt.plot(x_values, average_LLM, label='LLM')
 plt.fill_between(x_values, np.array(average_LLM) - np.array(std_LLM), np.array(average_LLM) + np.array(std_LLM), alpha=0.3)
 x_values = np.arange(len(data_BOPRO[0]))
-plt.plot(x_values, average_BOPRO, label='BOPRO')
-plt.fill_between(x_values, np.array(average_BOPRO) - np.array(std_BOPRO), np.array(average_BOPRO) + np.array(std_BOPRO), alpha=0.4)
+plt.plot(x_values, average_BOPRO, label='BOPRO',color='red')
+plt.fill_between(x_values, np.array(average_BOPRO) - np.array(std_BOPRO), np.array(average_BOPRO) + np.array(std_BOPRO), alpha=0.4,color='red')
+plt.xlabel('Iteration')
+plt.ylabel('Loss')
 plt.legend()
 plt.show()
 
